76167e5e3a5ffcef08ce77a45962732644d28e10
Håkon Østerbø <hauk88@gmail.com>
2012-09-20 10:33:33 +0200

    added vertical_motion* and updated Problem

---
+++ projects/parachute_jumper/vertical_motion_force.py
@@ -0,0 +1,32 @@
+from Problem import Problem
+from solver_vertical_motion import Solver
+from read_file import read_file
+from scitools.std import plot
+from numpy import linspace
+import sys
+
+v0 = 0
+T = 100
+dt = 0.01
+N = int(T/float(dt))
+
+parameters = read_file(sys.argv[1:])
+problem = Problem(parameters)
+problem.set_initial_condition(v0);
+
+c = Solver(problem)
+c.set_timestep(dt)
+v = c.solve(T)
+
+v = array(v)
+F_d = problem.get_drag_force(v)
+F_g = zeros(N) + problem.get_gravity_force()
+F_b = zeros(N) + problem.get_buoyancy_force()
+
+time = linspace(0,dt*N,N);
+
+plot(time,F_d,time,F_g,time,F_b)
+raw_input()
+
+
+
