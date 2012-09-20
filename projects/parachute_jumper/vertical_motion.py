d366013d2340665e26f5632627bd545ac5a68ae6
Waloeen <waloeen@gmail.com>
2012-09-20 13:02:28 +0200

    changed vertical_motion.py and added test_vertical_motion.py

--- projects/parachute_jumper/vertical_motion.py
+++ projects/parachute_jumper/vertical_motion.py
@@ -5,23 +5,28 @@ from scitools.std import plot
 from numpy import linspace
 import sys
 
-v0 = 0
-T = 100
-dt = 0.01
-N = int(T/float(dt))
 
-parameters = read_file(sys.argv[1:])
-problem = Problem(parameters)
-problem.set_initial_condition(v0);
+def init(v0, T, dt, N, parameters):
+	problem = Problem(parameters)
+	problem.set_initial_condition(v0);
 
-c = Solver(problem)
-c.set_timestep(dt)
-v = c.solve(T)
+	c = Solver(problem)
+	c.set_timestep(dt)
+	v = c.solve(T)
 
-time = linspace(0,dt*N,N);
-
-plot(time,v)
-raw_input()
+	time = linspace(0,dt*N,N);
+	return time, v
 
 
+if __name__ == '__main__':
+	parameters = read_file(sys.argv[1:])
+	v0 = 0
+	T = 100
+	dt = 0.01
+	N = int(T/float(dt))
+
+	time, v = init(v0, T, dt, N, parameters)
+
+	plot(time,v)
+	raw_input()
 
